e722ba42802d2e480574e7977b7045fef62a459b
Viorel <viorelbotezatu14@gmal.com>
2021-09-09 16:51:43 +0300

    Added Maven

---
+++ src/main/java/com/AutorisiCarti/AutorisiCartiApplication.java
@@ -0,0 +1,13 @@
+package com.AutorisiCarti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorisiCartiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorisiCartiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AutorisiCarti/AutorisiCartiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutorisiCarti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorisiCartiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
